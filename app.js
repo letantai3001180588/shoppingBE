c6d14f6decac69bc742443e3ad0e155ea802556a
letantai3001180588 <54881497+letantai3001180588@users.noreply.github.com>
2023-03-04 14:42:53 +0700

    update

--- app.js
+++ app.js
@@ -21,7 +21,13 @@ app.use(express.json())
 app.use(methodOverride('_method'))
 app.use(cookieParser())
 app.use(bodyParser.json());
-app.use(cors())
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 const Schema = mongoose.Schema;
 const acountSchema = new Schema({
