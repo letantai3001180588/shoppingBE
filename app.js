6154084d8012b3d7b804601750823d909c730871
letantai3001180588 <54881497+letantai3001180588@users.noreply.github.com>
2023-04-05 19:20:34 +0700

    commit

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const path = require('path')
 const methodOverride=require('method-override')
 const jwt=require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-var cors = require('cors')
+const cors = require('cors')
+const nodemailer =  require('nodemailer');
+
+
 
 const app = express()
 const port = 3000
@@ -55,6 +58,7 @@ const product=mongoose.model('product',productSchema)
 const billSchema = new Schema({
   id_user:{type:Schema.Types.ObjectId,ref:'acount'},
   total:{type:Number},
+  statusOrder:{type:Number},
   },{
     collection:'bill',
     timestamps:true
@@ -70,6 +74,37 @@ const detailBillSchema = new Schema({
     timestamps:true
   });
 const detailBill=mongoose.model('detailBill',detailBillSchema)
+
+const http = require("http");
+const { type } = require('os');
+const server = http.createServer(app);
+
+const socketIo = require("socket.io")(server, {
+  cors: {
+      origin: "*",
+  }
+});
+
+
+socketIo.on("connection", (socket) => {
+console.log("New client connected" + socket.id);
+
+  socket.emit("getId", socket.id);
+
+  socket.on("sendDataClient", function(data) {
+    console.log(data)
+    socketIo.emit("sendDataServer", { data });
+  })
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+  
+});
+server.listen(4000, () => {
+  console.log('Server đang chay tren cong 4000');
+});
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
@@ -144,7 +179,59 @@ app.get('/admin', async(req, res) => {
   }
 })
 
+app.put('/bill/transport/:id', (req, res) => {
+  const accessToken=req.header('Authorization')
+  if(!accessToken){
+    res.send('Ban can phai dang nhap')
+  }
+  else{
+    let verify=jwt.verify(accessToken,'mk')
+    acount.find({_id:verify._id}).then((data)=>{
+      const role=String(data[0].role)
+      if(role==='admin'){
+        bill.updateOne({_id:req.params.id},{$set:{statusOrder:1}})
+        .then(() => {
+          res.send('Access')
+        })
+        .catch()
+      }
+      else{
+        res.send('Failture')
+      }
+    }).catch(()=>{
+      res.send('Tai khoan hoac mat khau khong dung')
+    })
+  }
+})
 
+
+app.put('/acount/transport/:id', (req, res) => {
+  const accessToken=req.header('Authorization')
+  if(!accessToken){
+    res.send('Ban can phai dang nhap')
+  }
+  else{
+    let verify=jwt.verify(accessToken,'mk')
+    acount.find({_id:verify._id})
+    .then((userAcount)=>{
+      bill.find({id_user:verify._id})
+      .then((userBill)=>{
+        if(userAcount===userBill){
+          bill.updateOne({_id:req.params.id},{$set:{statusOrder:2}})
+          .then(() => {
+            res.send('Access')
+          })
+          .catch()
+            res.send('Failture')
+        }
+    })
+
+
+    }).catch(()=>{
+      res.send('Tai khoan hoac mat khau khong dung')
+    })
+  }
+})
 
 app.use('/acount/delete/:id', (req, res) => {
   const accessToken=req.header('Authorization')
@@ -196,10 +283,32 @@ app.put('/acount/update/:id', (req, res) => {
   }
 })
 
-app.get('/product',(req,res)=>{
-  product.find({})
-  .then((product)=>{
-    res.send(product)
+app.get('/product/:page', (req, res) => {
+  let perPage = 12;
+  let page = req.params.page || 1; 
+
+  product
+    .find() 
+    .skip((perPage * page) - perPage) 
+    .limit(perPage)
+    .then((products) => {
+      res.send(products)
+  });
+
+
+});
+app.get('/sumPage', async (req,res)=>{
+  const estimate = await product.estimatedDocumentCount();
+  res.send(String(estimate/12))
+})
+
+app.get('/search/:product', async (req,res)=>{
+  let search=req.params.product
+  let query={}
+  query.name = new RegExp(search,'i')
+  product.find(query).limit(12)
+  .then((a)=>{
+    res.send(a)
   })
 })
 
@@ -276,8 +385,93 @@ app.put('/product/update/:id', (req, res) => {
   }
 })
 
+app.get('/bill/waitOrder/:id_user', (req, res) => {
+  bill.find({id_user:req.params.id_user,statusOrder:0})
+  .then((data)=>{
+    res.send(data)
+  })
+})
+
+app.get('/bill/transportOrder/:id_user', (req, res) => {
+  bill.find({id_user:req.params.id_user,statusOrder:1})
+  .then((data)=>{
+    res.send(data)
+  })
+})
+
+app.get('/bill/finishOrder/:id_user', (req, res) => {
+  bill.find({id_user:req.params.id_user,statusOrder:2})
+  .then((data)=>{
+    res.send(data)
+  })
+})
+
 app.post('/bill/create',(req,res)=>{
   const accessToken=req.header('Authorization')
+  const transporter =  nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'taile07032000@gmail.com', 
+        pass: 'ywyzwofihevekaes' 
+    }
+  });
+  let style=`
+    style="border: 1px solid #dddddd;padding: 8px;"
+  `
+  let head=`
+    <table style="width: 100%;">
+      <thead>
+        <tr >
+          <th ${style} scope="col">ID</th>
+          <th ${style} scope="col">Image</th>
+          <th ${style} scope="col">Name</th>
+          <th ${style} scope="col">Amount</th>
+        </tr>
+      </thead>
+      <tbody >
+  `
+  
+  let foot=`
+      </tbody>
+    </table>
+  `
+
+  const img=req.body.detailBill[0]
+  const name=req.body.detailBill[1]
+  const amount=req.body.detailBill[2]
+
+  let content=req.body.detailBill[0].map((item,i)=>{
+    return`
+      <tr>
+          <th ${style}>${i+1}</th>
+          <td ${style}>
+              <img style="width: 50px;height: 50px;" 
+              src="${img[i]}"/>
+          </td>
+          <td ${style}>
+            ${name[i]}
+          </td>
+          <td ${style}>
+            ${amount[i]}
+          </td>
+      </tr>
+      `;
+
+  })
+
+  const html=head+String(content.join(''))+foot;
+  const mainOptions = { 
+      from: 'NQH-Test nodemailer',
+      to: req.body.email,
+      subject: 'Danh sách đặt hàng từ Shopping',
+      text: '',
+      html: html
+  }
+  
+  console.log(req.body.email||'Not found')
+  
   if(!accessToken){
     res.send('Ban can phai dang nhap')
   }
@@ -285,10 +479,15 @@ app.post('/bill/create',(req,res)=>{
     let verify=jwt.verify(accessToken,'mk')
     acount.find({_id:verify._id}).then((data)=>{
       const bill=mongoose.model('bill',billSchema)
-      const newBill= new bill(req.body)
+      const newBill= new bill(req.body.bill)
       newBill.save()
-      .then((item) => {
+      .then((item) => { 
         res.send(item)
+      })
+      .then(()=>{
+        transporter.sendMail(mainOptions, function(err, info){
+          err?res.send(err):console.log(info)
+        });
       })
       .catch()
     }).catch(()=>{
@@ -296,6 +495,7 @@ app.post('/bill/create',(req,res)=>{
     })
   }
 })
+
 
 app.use('/bill/delete/:id',(req,res)=>{
   const accessToken=req.header('Authorization')
